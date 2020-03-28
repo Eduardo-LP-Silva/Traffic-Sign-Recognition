e947b277980ef2f5b18227a3b3152289d862ae27
Joana Ramos <joanasmramos98@gmail.com>
2020-03-28 22:19:02 +0000

    [FEATURE] Segmentation by color (blue) with thresholding

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import cv2 as cv
 import numpy as np
 
-# It's missing the option to take the picture
-# Read from file
+# TO DO: option to take the picture
+# read from file
 #filename = input('Filename: ')
 #img = cv.imread(filename)
-img = cv.imread('./examples/exemplo1.jpg', 0)
+
+img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
+# hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
 cv.imshow('Traffic signs', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-edges = cv.Canny(img,200,255)
-cv.imshow('edges', edges)
+# shades of blue (light and dark)
+light_blue = (255, 150, 60)
+dark_blue = (100, 0, 0)
+
+# all values that are not in the above range will be black
+mask = cv.inRange(img, dark_blue, light_blue)
+result = cv.bitwise_and(img, img, mask=mask)
+
+# show the result
+cv.imshow('Thresholding blue', result)
 cv.waitKey(0)
 cv.destroyAllWindows()
-
-# Is noise smoothing necessary?
-# Edge detection
-# Shape detection -> squares/triangles/circles
-# Color detection -> blue/red
-
-# -> Sign detection
