3a523412f653110988e703a8e8abba2ec790ca88
Joana Ramos <joanasmramos98@gmail.com>
2020-03-29 00:20:12 +0000

    [FEATURE] Add Canny edge detector and findContours

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import numpy as np
 
 # TO DO: option to take the picture
 # read from file
-#filename = input('Filename: ')
-#img = cv.imread(filename)
+filename = input('Filename: ')
+img = cv.imread(filename)
 
-img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
-# hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
 
 cv.imshow('Traffic signs', img)
 cv.waitKey(0)
@@ -23,5 +22,22 @@ result = cv.bitwise_and(img, img, mask=mask)
 
 # show the result
 cv.imshow('Thresholding blue', result)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# applying Canny edge detector
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_edges_enhanced = cv.Canny(img, 150, 200)
+
+cv.imshow('Canny edge detector', img_edges_enhanced)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# detecting contours
+# contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+img_contours = cv.drawContours(img, contours, -1, (0,255,0), 1)
+cv.imshow('Image contours', img_contours)
 cv.waitKey(0)
 cv.destroyAllWindows()
