f6c071588e4156bbc5f0060b559913dc580870b0
Joana Ramos <joanasmramos98@gmail.com>
2020-03-30 16:18:51 +0100

    [FEATURE] weird rectangles but ok

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def detect_shape(contour):
 
 
 # TO DO: option to take the picture
-# read from file
-filename = input('Filename: ')
-img = cv.imread(filename)
+# read from keyboard the name of the image
+# filename = input('Filename: ')
+# img = cv.imread(filename)
 
-# img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
+img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
 display_image('Original image', img)
 
 ###### COLOR #######
@@ -50,18 +50,40 @@ result = cv.bitwise_and(img, img, mask=mask)
 # applying Canny edge detector
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blurred = cv.GaussianBlur(gray_img, (5, 5), 0)
-display_image('Blurred', blurred)
+# display_image('Blurred', blurred)
 img_edges_enhanced = cv.Canny(blurred, 100, 200)
 
 # show the result of applying Canny
-display_image('Canny edge detector', img_edges_enhanced)
+# display_image('Canny edge detector', img_edges_enhanced)
 
 # detecting contours
 # contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 contours, _ = cv.findContours(img_edges_enhanced, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)    
 
 # show the result contours (60)
-img_contours = cv.drawContours(img, contours, -1, (0,255,0), 1)
+# img_contours = cv.drawContours(img, contours, -1, (0,255,0), 1)
 
 # show the image contours
-display_image('Image contours', img_contours)
+# display_image('Image contours (in green)', img_contours)
+
+# loop over the contours
+for c in contours:
+    # compute the center of the contour, then detect the name of the shape using only the contour
+    M = cv.moments(c)
+    # cX = int((M["m10"] / M["m00"]) * ratio)
+    # cY = int((M["m01"] / M["m00"]) * ratio)
+    if M['m00'] == 0:
+        continue
+
+    shape = detect_shape(c)
+    print(shape)
+    if shape != 'rectangle' and shape != 'square':
+        continue
+
+    cv.drawContours(img, [c], -1, (0, 255, 0), 2)
+    cX = c[0,0,0]
+    cY = c[0,0,1]
+    cv.putText(img, shape, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    # display_image('Shape', img)
+
+display_image('Shapes', img)
