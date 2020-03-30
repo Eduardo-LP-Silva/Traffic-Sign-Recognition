0ab5d3299c49c64e4024525f5fd57adf7b7ea33c
Joana Ramos <joanasmramos98@gmail.com>
2020-03-30 15:32:18 +0100

    [FEATURE] Add blurring and some utility functions

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import cv2 as cv
 import numpy as np
 
+def display_image(window_name, img):
+    cv.imshow(window_name, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def detect_shape(contour):
+    shape = 'unidentified'
+    # parameters: curve, closed
+    peri = cv.arcLength(contour, True) # perimeter
+    # parameters: curve, epsilon (approximation accuracy), closed
+    approx = cv.approxPolyDP(contour, 0.04 * peri, True)
+    # if the shape has 4 vertices, it is either a square or a rectangle
+    if len(approx) == 4:
+        # compute the bounding box of the contour and use it to compute the aspect ratio
+        (x, y, w, h) = cv.boundingRect(approx)
+        ar = w / float(h)
+
+        # a square will have an aspect ratio that is approximately equal to one, otherwise, the shape is a rectangle
+        shape = 'square' if ar >= 0.95 and ar <= 1.05 else 'rectangle'
+    
+    return shape
+
+
 # TO DO: option to take the picture
 # read from file
 filename = input('Filename: ')
 img = cv.imread(filename)
 
 # img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
+display_image('Original image', img)
 
-cv.imshow('Traffic signs', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+###### COLOR #######
+# Maybe change to HSV?
 
 # shades of blue (light and dark)
 light_blue = (255, 150, 60)
@@ -20,24 +43,25 @@ dark_blue = (100, 0, 0)
 mask = cv.inRange(img, dark_blue, light_blue)
 result = cv.bitwise_and(img, img, mask=mask)
 
-# show the result
-cv.imshow('Thresholding blue', result)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# display_image('Thresholding blue', result)
+
+###### SHAPE #######
 
 # applying Canny edge detector
 gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-img_edges_enhanced = cv.Canny(img, 150, 200)
+blurred = cv.GaussianBlur(gray_img, (5, 5), 0)
+display_image('Blurred', blurred)
+img_edges_enhanced = cv.Canny(blurred, 100, 200)
 
-cv.imshow('Canny edge detector', img_edges_enhanced)
-cv.waitKey(0)
-cv.destroyAllWindows()
+# show the result of applying Canny
+display_image('Canny edge detector', img_edges_enhanced)
 
 # detecting contours
 # contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours, _ = cv.findContours(img_edges_enhanced, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)    
 
+# show the result contours (60)
 img_contours = cv.drawContours(img, contours, -1, (0,255,0), 1)
-cv.imshow('Image contours', img_contours)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+# show the image contours
+display_image('Image contours', img_contours)
