29077dc470f734b21c7f5d9d123517fbd3c533dd
Joana Ramos <joanasmramos98@gmail.com>
2020-04-04 17:09:29 +0100

    [FIX] Fix gaussian blur

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ def detect_shape(contour):
 
 # TO DO: option to take the picture
 # read from keyboard the name of the image
-# filename = input('Filename: ')
-# img = cv.imread(filename)
+filename = input('Filename: ')
+img = cv.imread(filename)
 
-img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
+# img = cv.imread('./examples/blue-rectangles/1.jpg') # retorna Mat
 display_image('Original image', img)
+blurred = cv.bilateralFilter(img, 5, 75, 75)
+# display_image('Blurred', blurred)
 
 ###### COLOR #######
 # Maybe change to HSV?
@@ -40,21 +42,21 @@ light_blue = (255, 150, 60)
 dark_blue = (100, 0, 0)
 
 # all values that are not in the above range will be black
-mask = cv.inRange(img, dark_blue, light_blue)
-result = cv.bitwise_and(img, img, mask=mask)
+mask = cv.inRange(blurred, dark_blue, light_blue)
+result = cv.bitwise_and(blurred, blurred, mask=mask)
 
-# display_image('Thresholding blue', result)
+display_image('Thresholding blue', result)
 
 ###### SHAPE #######
 
 # applying Canny edge detector
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blurred = cv.GaussianBlur(gray_img, (5, 5), 0)
-# display_image('Blurred', blurred)
-img_edges_enhanced = cv.Canny(blurred, 100, 200)
+gray_img = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
+# img_edges_enhanced = cv.Canny(gray_img, 100, 200)
 
 # show the result of applying Canny
 # display_image('Canny edge detector', img_edges_enhanced)
+
+_, img_edges_enhanced = cv.threshold(gray_img, 1, 255, cv.THRESH_BINARY)
 
 # detecting contours
 # contours, hierarchy = cv.findContours(img_edges_enhanced, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -63,27 +65,24 @@ contours, _ = cv.findContours(img_edges_enhanced, cv.RETR_EXTERNAL, cv.CHAIN_APP
 # show the result contours (60)
 # img_contours = cv.drawContours(img, contours, -1, (0,255,0), 1)
 
-# show the image contours
-# display_image('Image contours (in green)', img_contours)
-
 # loop over the contours
 for c in contours:
     # compute the center of the contour, then detect the name of the shape using only the contour
     M = cv.moments(c)
-    # cX = int((M["m10"] / M["m00"]) * ratio)
-    # cY = int((M["m01"] / M["m00"]) * ratio)
     if M['m00'] == 0:
         continue
 
     shape = detect_shape(c)
-    print(shape)
     if shape != 'rectangle' and shape != 'square':
         continue
+
+    if cv.contourArea(c) < 1000:
+        continue
+    print(cv.contourArea(c))
 
     cv.drawContours(img, [c], -1, (0, 255, 0), 2)
     cX = c[0,0,0]
     cY = c[0,0,1]
     cv.putText(img, shape, (cX, cY), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-    # display_image('Shape', img)
 
 display_image('Shapes', img)
